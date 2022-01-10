2e6f3d1da8c60ff2e645691f2ceca5085e960a26
Alfio Martini <alfio.martini@gmail.com>
2022-01-10 15:26:36 -0300

    Implemented basic login, and  movements's panel rendering

---
+++ src/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import { updateMovements, logIn } from "./utils.js";
+import { accounts } from "./script.js";
+
+// Dom Elements
+
+const inputName = document.querySelector(".header__username");
+const inputPin = document.querySelector(".header__pin");
+const logBtn = document.querySelector(".header__btn");
+const movementsElm = document.querySelector(".movements");
+
+function updateUI(account) {
+  const movementsStr = updateMovements(account);
+  movementsElm.innerHTML = movementsStr;
+}
+
+logBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const userName = inputName.value;
+  const userPin = inputPin.value;
+  const loggedUser = logIn(userName, userPin);
+  if (loggedUser) updateUI(loggedUser.account);
+});
---
+++ src/utils.js
@@ -0,0 +1,47 @@
+//  functions
+import { accounts } from "./script.js";
+
+export function updateMovements(account) {
+  const { movements } = account;
+  let strMov = "";
+  movements.forEach((mov) => {
+    const date = new Date().toLocaleDateString();
+    const type = mov < 0 ? "withdrawal" : "deposit";
+    const value = mov.toString() + ".00";
+    const movement = `
+    <span class="movements__type movements--${type}">${type}</span>
+    <span class="movements__date">${date}</span>
+    <span class="movements__value">${value}</span>
+    `;
+    strMov += movement;
+  });
+  return strMov;
+}
+
+export function logIn(inputName, inputPin) {
+  inputPin = parseInt(inputPin);
+  const account = accounts.find((account) => {
+    return account.pin === inputPin;
+  });
+  if (!account) {
+    console.log("user not found", account);
+    return null;
+  }
+  let { owner, pin } = account;
+  const username = owner
+    .split(" ")
+    .map((name) => name[0])
+    .join("")
+    .toLowerCase();
+  if (inputPin === pin && username === inputName) {
+    console.log("authentication successful");
+    return {
+      username,
+      pin,
+      account,
+    };
+  } else {
+    console.log("error in authentication");
+    return null;
+  }
+}
